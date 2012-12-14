31344a06dbe98d63930d31d4105668a1aaa58e3b
Ando <dragos@txtfeedback.net>
2012-12-14 14:37:57 +0200

    Removed purchases & purchaseTypes

--- iloire Facturacion/Models/DBContext.cs
+++ iloire Facturacion/Models/DBContext.cs
@@ -10,14 +10,14 @@ public class InvoiceDB : smsfeedbackEntities {
     {}
 
     //public DbSet<Customer> Customers {get; set;}
-    public DbSet<Provider> Providers { get; set; }
+    //public DbSet<Provider> Providers { get; set; }
     //public DbSet<InvoiceVM> Invoices
     //{
     //   get;
     //   set;
     //}
-    public DbSet<Purchase> Purchases { get; set; }
-    public DbSet<PurchaseType> PurchaseTypes { get; set; }
+    //public DbSet<Purchase> Purchases { get; set; }
+    //public DbSet<PurchaseType> PurchaseTypes { get; set; }
     //public DbSet<InvoiceDetailsVM> InvoiceDetails { get; set; }
 
     //public DbSet<User> Users { get; set; }
