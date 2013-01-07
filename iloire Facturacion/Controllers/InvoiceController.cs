01c56d19a472e0934e636d3982d1d00bd8ec1e61
Ando <dragos@txtfeedback.net>
2013-01-07 17:33:24 +0200

    Added tracing

--- iloire Facturacion/Controllers/InvoiceController.cs
+++ iloire Facturacion/Controllers/InvoiceController.cs
@@ -285,8 +285,8 @@ namespace iloire_Facturacion.Controllers
                    foreach (var validationErrors in dbEx.EntityValidationErrors)
                    {
                       foreach (var validationError in validationErrors.ValidationErrors)
-                      {
-                         Trace.TraceInformation("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                      {                         
+                         logger.ErrorFormat("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
                       }
                    }
                 }
@@ -540,8 +540,7 @@ namespace iloire_Facturacion.Controllers
               foreach (var validationErrors in dbEx.EntityValidationErrors)
               {
                  foreach (var validationError in validationErrors.ValidationErrors)
-                 {
-                    //Trace.TraceInformation("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                 {                    
                     logger.ErrorFormat("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
                  }
               }
